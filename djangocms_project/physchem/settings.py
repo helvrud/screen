afd59aeb325460a2bfd788ae5a299b6b419d1b1a
O.V. Rud <oleg.rud@natur.cuni.cz>
2026-01-15 01:28:14 +0100

    static

--- djangocms_project/physchem/settings.py
+++ djangocms_project/physchem/settings.py
@@ -141,7 +141,7 @@ SITE_ID = 1
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = '/screen/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
